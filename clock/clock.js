626d80ff40283b3ae40ee0a394e4bfeb6fb03e15
Benyamin Ahmed <benyaminahmed2@gmail.com>
2018-02-13 12:27:06 +0000

    I uploaded a clock

---
+++ clock/clock.js
@@ -0,0 +1,20 @@
+function clock() {
+    var time = new Date();
+
+    hours = time.getHours();
+
+    minutes = time.getMinutes();
+
+    seconds = time.getSeconds();
+
+    document.querySelectorAll('.clock')[0].innerHTML = harold(hours) + ":" + harold(minutes) + ":" + harold(seconds);
+
+    function harold(standIn) {
+        if (standIn < 10) {
+            standIn = '0' + standIn
+        }
+        return standIn;
+    }
+}
+setInterval(clock, 1000);
+
